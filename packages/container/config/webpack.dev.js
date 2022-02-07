6f76535b37eec6ee83a0e324369bba2a0c5c387b
omgpiu <dyba@list.ru>
2022-02-07 15:01:10 +0300

    webpack configs

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,26 @@
+const {merge} = require('webpack-merge')
+const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
+
+//shared modules  через packageJson  не работает, бандл увеличивается, не понятна причина
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+            },
+            shared: packageJson.dependencies
+        }),
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig)
---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MarketingApp from "./components/MarketingApp";
+
+
+const App = () => {
+    return <div>
+        <h2>Hehey</h2>
+        <MarketingApp/>
+    </div>
+}
+
+export default App
