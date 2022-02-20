982abc327f813caebadeb502505523ee2d4a9831
omgpiu <dyba@list.ru>
2022-02-20 22:44:50 +0300

    feat: add dashboard flow

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'board',
+            name: 'dashboard',
             filename: 'remoteEntry.js',
             exposes: {
                 './DashboardApp': './src/bootstrap',
